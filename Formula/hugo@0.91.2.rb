c92f1e660866837d74b2a8950cb6df8a383a153e
bapt <bapt@Bapt-MBP.local>
2022-03-17 11:15:35 +0100

    init

---
+++ Formula/hugo@0.91.2.rb
@@ -0,0 +1,37 @@
+class HugoAT0912 < Formula
+  desc "Configurable static site generator"
+  homepage "https://gohugo.io/"
+  url "https://github.com/gohugoio/hugo/archive/v0.91.2.tar.gz"
+  sha256 "a749485225d682dee43ea6a0644d5bd2e587c0535508be90e679b21e4553f8e9"
+  license "Apache-2.0"
+  head "https://github.com/gohugoio/hugo.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "-tags", "extended"
+
+    # Install bash completion
+    output = Utils.safe_popen_read(bin/"hugo", "completion", "bash")
+    (bash_completion/"hugo").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read(bin/"hugo", "completion", "zsh")
+    (zsh_completion/"_hugo").write output
+
+    # Install fish completion
+    output = Utils.safe_popen_read(bin/"hugo", "completion", "fish")
+    (fish_completion/"hugo.fish").write output
+
+    # Build man pages; target dir man/ is hardcoded :(
+    (Pathname.pwd/"man").mkpath
+    system bin/"hugo", "gen", "man"
+    man1.install Dir["man/*.1"]
+  end
+
+  test do
+    site = testpath/"hops-yeast-malt-water"
+    system "#{bin}/hugo", "new", "site", site
+    assert_predicate testpath/"#{site}/config.toml", :exist?
+  end
+end
